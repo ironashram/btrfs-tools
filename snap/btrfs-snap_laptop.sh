e044c1d4b69bf91f5ff193ba7bf68ee5a6dc41c1
Michele Palazzi <sysdadmin@m1k.cloud>
2025-11-20 14:50:55 +0100

    chore: add systemd-boot update to laptops too
    Signed-off-by: Michele Palazzi <sysdadmin@m1k.cloud>

--- snap/btrfs-snap_laptop.sh
+++ snap/btrfs-snap_laptop.sh
@@ -6,3 +6,7 @@ sync
 
 # Delete snapshotshots older than 7 days
 find /* -maxdepth 0 -mtime +7 -name "root-snapshot.*" | xargs btrfs subvolume delete 
+
+# Update systemd-boot snapshots
+systemctl restart update-systemd-boot-snapshots.service
+
